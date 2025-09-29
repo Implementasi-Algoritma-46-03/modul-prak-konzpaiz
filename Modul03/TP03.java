60b25bf0960c2554a59384285d8f9d4f9b4fbd62
faizrizqullah <faizrizqullah>
2025-09-29 14:19:07 +0700

    Faiz Rizqullah Tugas TP

--- Modul03/TP03.java
+++ Modul03/TP03.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class TP03 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
-    }
+        Scanner input = new Scanner(System.in);
+        
+        int N = input.nextInt();
+        int T = input.nextInt();
+        
+        int totalOrang = T + 1;
+        
+        int jatah = N / totalOrang;
+        int sisa = N % totalOrang;
+        
+        //Output
+        System.out.println(jatah);
+        System.out.println(sisa);
+        
+        input.close();
+          }
 }

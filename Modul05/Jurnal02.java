3821eafdd56a0bd174a298e99ca337d2701d40b2
faizrizqullah <faizrizqullah>
2025-10-16 18:31:26 +0700

    modul 4

--- Modul05/Jurnal02.java
+++ Modul05/Jurnal02.java
@@ -1,6 +1,43 @@
-public class Jurnal02 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class Jurnal02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String hari = sc.next();
+        int n = sc.nextInt();
+
+        String[] daftarhari = {"Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"};
+
+        int posisi = 0;
+
+        switch(hari){
+            case "Senin":
+                posisi = 0;
+                break;
+            case "Selasa":
+                posisi = 1;
+                break;
+            case "Rabu":
+                posisi = 2;
+                break;
+            case "Kamis":
+                posisi = 3;
+                break;
+            case "Jumat":
+                posisi = 4;
+                break;
+            case "Sabtu":
+                posisi = 5;
+                break;
+            case "Minggu":
+                posisi = 6;
+                break;
+            default:
+                System.out.println("Hari tidak valid");
+                return;
+        }
+
+        int hasil = (posisi + n) % 7;
+        System.out.println(daftarhari[hasil]);
     }
 }
--- Modul05/Jurnal03.java
+++ Modul05/Jurnal03.java
@@ -1,6 +1,29 @@
-public class Jurnal03 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class Jurnal03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double angka1 = sc.nextInt();
+        char op = sc.next().charAt(0);
+        double angka2 = sc.nextInt();
+
+        switch(op){
+            case '+':
+                System.out.println((int)angka1 + angka2);
+                break;
+            case '-':
+                System.out.println((int)angka1 - angka2);
+                break;
+            case '*':
+                System.out.println((int)angka1 * angka2);
+                break;
+            case '/':
+                double hasilBagi = angka1 / angka2; 
+                System.out.printf("%.7f",hasilBagi);
+                break;
+            default:
+                System.out.println("Operator tidak valid");
+                break;
+        }
     }
 }

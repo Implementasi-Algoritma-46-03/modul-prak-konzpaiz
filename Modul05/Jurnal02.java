098b7f8ea94d489722fe57e7dba5158438523c8c
faizrizqullah <faizrizqullah>
2025-10-16 18:57:31 +0700

    update

--- Modul05/Jurnal02.java
+++ Modul05/Jurnal02.java
@@ -1,43 +1,55 @@
 import java.util.Scanner;
 
 public class Jurnal02 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String hari = sc.next();
-        int n = sc.nextInt();
+    public static void main(final String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        String hari = input.nextLine();
+        int berapaHari = input.nextInt();
 
-        String[] daftarhari = {"Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"};
-
-        int posisi = 0;
-
-        switch(hari){
+        int indexHariSekarang = 0;
+        switch (hari) {
             case "Senin":
-                posisi = 0;
+                indexHariSekarang = 0;
                 break;
             case "Selasa":
-                posisi = 1;
+                indexHariSekarang = 1;
                 break;
             case "Rabu":
-                posisi = 2;
+                indexHariSekarang = 2;
                 break;
             case "Kamis":
-                posisi = 3;
+                indexHariSekarang = 3;
                 break;
             case "Jum'at":
-                posisi = 4;
+                indexHariSekarang = 4;
                 break;
             case "Sabtu":
-                posisi = 5;
+                indexHariSekarang = 5;
                 break;
             case "Minggu":
-                posisi = 6;
+                indexHariSekarang = 6;
                 break;
-            default:
-                System.out.println("Hari tidak valid");
-                return;
         }
 
-        int hasil = (posisi + n) % 7;
-        System.out.println(daftarhari[hasil]);
+
+        int indexHariSetelahN = (indexHariSekarang + berapaHari) % 7;
+
+        switch (indexHariSetelahN) {
+            case 0: System.out.println("Senin");
+                break;
+            case 1: System.out.println("Selasa");
+                break;
+            case 2: System.out.println("Rabu");
+                break;
+            case 3: System.out.println("Kamis");
+                break;
+            case 4: System.out.println("Jum'at");
+                break;
+            case 5: System.out.println("Sabtu");
+                break;
+            case 6: System.out.println("Minggu");
+                break;
+        }
     }
 }

60fa8a0c1419ab4388fda00a13fd7c0e07498ef1
faizrizqullah <faizrizqullah>
2025-10-23 16:09:31 +0700

    update

--- Modul06/TP01.java
+++ Modul06/TP01.java
@@ -3,29 +3,25 @@ import java.util.Scanner;
 public class TP01 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
-        Scanner sc = new Scanner(System.in);
-        double r1 = sc.nextDouble();
-        double r2 = sc.nextDouble();
-        double r3 = sc.nextDouble();
+        Scanner input = new Scanner(System.in);
 
-        double c1 = konversiCelcius(r1);
-        double c2 = konversiCelcius(r2);
-        double c3 = konversiCelcius(r3);
+        double in1 = input.nextDouble();
+        double in2 = input.nextDouble();
+        double in3 = input.nextDouble();
 
-        double re1 = konversiReamur(r1);
-        double re2 = konversiReamur(r2);
-        double re3 = konversiReamur(r3);
-
-        System.out.printf("%.2f %.2f %.2f\n", c1, c2, c3);
-        System.out.printf("%.2f %.2f %.2f\n", re1, re2, re3);
+        System.out.print("Celcius: ");
+        System.out.printf("%.1f %.1f %.1f", selsius(in1), selsius(in2), selsius(in3));
+        System.out.print("\nReamur: ");
+        System.out.printf("%.2f %.2f %.2f,", reamur(in1), reamur(in2), reamur(in3));
     }
 
-    public static double konversiCelcius(double f){
-        return (f - 32) * 5 / 9;
+    public static double selsius(double a) {
+        double rumus = (a - 32) * 5/9;
+        return rumus;
     }
 
-    public static double konversiReamur(double f){
-        return (f - 32) * 4 / 9;
-    }
+    public static double reamur(double a) {
+        double hasil = (a - 32) * 4/9;
+        return hasil;
+    }
 }

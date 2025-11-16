8e9a6b955a19b17f77e9167fb63920efbb2fbf23
faizrizqullah <faizrizqullah>
2025-11-16 23:22:05 +0700

    pais TP

--- Modul10/TP02.java
+++ Modul10/TP02.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class TP02 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner x = new Scanner(System.in);
+
+        int n = x.nextInt();
+        int k = 0;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(j==i || j==n-i+1){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
     }
 }
--- Modul10/TP03.java
+++ Modul10/TP03.java
@@ -2,5 +2,21 @@ public class TP03 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        for(int a=1;a<=100;a++){
+            int cek = 0;
+
+            if(a>1){
+                for(int b=1;b<=a;b++){
+                    if(a%b==0){
+                        cek++;
+                    }
+                }
+                if(cek==2){
+                    System.out.print(a+" ");
+                }
+            }
+
+        }
+
     }
 }

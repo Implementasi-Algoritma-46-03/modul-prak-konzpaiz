a49d6ad4e9677b999489ed23d607730e565e33d0
faizrizqullah <faizrizqullah>
2025-11-20 16:13:15 +0700

    UPDATE 2

--- Modul10/TP02.java
+++ Modul10/TP02.java
@@ -1,23 +1,29 @@
 import java.util.Scanner;
+
 public class TP02 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
-       Scanner x = new Scanner(System.in);
 
-        int n = x.nextInt();
-        int k = 0;
+        Scanner in = new Scanner(System.in);
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(j==i || j==n-i+1){
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
+        int masukan = in.nextInt();
+
+        // Bagian naik
+        for (int i = 1; i <= masukan ; i++){
+            for (int j = 1; j < i; j++){
+                System.out.print(" ");
             }
-            System.out.println();
+            System.out.println(i);  
         }
 
+        // Bagian turun
+        for (int i = masukan - 1; i >= 1; i--){
+            for (int s = 1; s < i; s++){
+                System.out.print(" ");
+            }
+            System.out.println(i);
+        }
+
+        in.close();
     }
 }

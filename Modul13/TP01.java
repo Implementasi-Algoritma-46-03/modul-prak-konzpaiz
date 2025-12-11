2b87c451cb225247150c0a81273516b05cebb53a
faizrizqullah <faizrizqullah>
2025-12-11 17:10:24 +0700

    UPDATE TP PAIS

--- Modul13/TP01.java
+++ Modul13/TP01.java
@@ -1,47 +1,45 @@
-import java.util.*;
+import java.util.Scanner;
+public class TP01{
+ public static void main(String[] args){
+  Scanner s = new Scanner(System.in);
+  double a[] = new double[10];
+  double b[] = new double[10];
+  String tok;
 
-public class TP01 {
+  
+  for(int i=0;i<10;i++){
+   tok = s.next();
+   a[i] = Double.parseDouble(tok.replace(',','.'));
+  }
+  
+  for(int i=0;i<10;i++){
+   tok = s.next();
+   b[i] = Double.parseDouble(tok.replace(',','.'));
+  }
 
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-
-        double[] a = new double[10];
-        double[] b = new double[10];
-
-        for(int i=0;i<10;i++){
-            a[i] = s.nextDouble();
-        }
-
-        for(int i=0;i<10;i++){
-            b[i] = s.nextDouble();
-        }
-
-        // bubble sort biasa (maba style)
-        for(int i=0;i<10;i++){
-            for(int j=0;j<9;j++){
-                if(a[j] > a[j+1]){
-                    double tmp = a[j];
-                    a[j] = a[j+1];
-                    a[j+1] = tmp;
-                }
-                if(b[j] > b[j+1]){
-                    double tmp2 = b[j];
-                    b[j] = b[j+1];
-                    b[j+1] = tmp2;
-                }
-            }
-        }
-
-        // print hasil
-        for(int i=0;i<10;i++){
-            System.out.print(a[i]);
-            if(i<9) System.out.print(" ");
-        }
-        System.out.println();
-
-        for(int i=0;i<10;i++){
-            System.out.print(b[i]);
-            if(i<9) System.out.print(" ");
-        }
+  
+  for(int i=0;i<10;i++){
+   for(int j=0;j<9;j++){
+    if(a[j] > a[j+1]){
+     double t=a[j]; a[j]=a[j+1]; a[j+1]=t;
     }
+    if(b[j] > b[j+1]){
+     double t=b[j]; b[j]=b[j+1]; b[j+1]=t;
+    }
+   }
+  }
+
+  
+  for(int i=0;i<10;i++){
+   System.out.print(String.format("%.2f", a[i]));
+   if(i<9) System.out.print(" ");
+  }
+  System.out.println();
+
+  
+  for(int i=0;i<10;i++){
+   System.out.print(String.format("%.2f", b[i]));
+   if(i<9) System.out.print(" ");
+  }
+ }
 }
--- Modul13/TP02.java
+++ Modul13/TP02.java
@@ -1,45 +1,40 @@
-import java.util.*;
+import java.util.Scanner;
+public class TP02{
+ public static void main(String[] args){
+  Scanner s = new Scanner(System.in);
+  double p[] = new double[10];
+  double q[] = new double[10];
+  String tok;
 
-public class TP02 {
+  for(int i=0;i<10;i++){
+   tok = s.next();
+   p[i] = Double.parseDouble(tok.replace(',','.'));
+  }
+  for(int i=0;i<10;i++){
+   tok = s.next();
+   q[i] = Double.parseDouble(tok.replace(',','.'));
+  }
 
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-
-        double[] x = new double[10];
-        double[] y = new double[10];
-
-        for(int i=0;i<10;i++){
-            x[i] = s.nextDouble();
-        }
-        for(int i=0;i<10;i++){
-            y[i] = s.nextDouble();
-        }
-
-        // bubble sort descending (paling lama)
-        for(int i=0;i<10;i++){
-            for(int j=0;j<9;j++){
-                if(x[j] < x[j+1]){
-                    double tmp = x[j];
-                    x[j] = x[j+1];
-                    x[j+1] = tmp;
-                }
-                if(y[j] < y[j+1]){
-                    double tmp2 = y[j];
-                    y[j] = y[j+1];
-                    y[j+1] = tmp2;
-                }
-            }
-        }
-
-        for(int i=0;i<10;i++){
-            System.out.print(x[i]);
-            if(i<9) System.out.print(" ");
-        }
-        System.out.println();
-
-        for(int i=0;i<10;i++){
-            System.out.print(y[i]);
-            if(i<9) System.out.print(" ");
-        }
+  // bubble sort descending (besar ke kecil)
+  for(int i=0;i<10;i++){
+   for(int j=0;j<9;j++){
+    if(p[j] < p[j+1]){
+     double t=p[j]; p[j]=p[j+1]; p[j+1]=t;
     }
+    if(q[j] < q[j+1]){
+     double t=q[j]; q[j]=q[j+1]; q[j+1]=t;
+    }
+   }
+  }
+
+  for(int i=0;i<10;i++){
+   System.out.print(String.format("%.2f", p[i]));
+   if(i<9) System.out.print(" ");
+  }
+  System.out.println();
+  for(int i=0;i<10;i++){
+   System.out.print(String.format("%.2f", q[i]));
+   if(i<9) System.out.print(" ");
+  }
+ }
 }

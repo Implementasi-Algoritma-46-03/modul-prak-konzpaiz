9cdafaa723ddda8752b714413cc167d3db663cf5
faizrizqullah <faizrizqullah>
2025-12-11 17:02:32 +0700

    update jurnal pais

--- Modul13/Jurnal01.java
+++ Modul13/Jurnal01.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
+
 public class Jurnal01 {
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+        String arr[] = new String[n];
+
+        for(int i=0;i<n;i++){
+            arr[i] = s.next();
+        }
+
+        for(int i=1;i<n;i++){
+            String x = arr[i];
+            int j = i-1;
+
+            while(j>=0 && arr[j].compareTo(x) < 0){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = x;
+        }
+
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]);
+            if(i<n-1) System.out.print(" ");
+        }
     }
 }
--- Modul13/Jurnal03.java
+++ Modul13/Jurnal03.java
@@ -1,6 +1,49 @@
+import java.util.Scanner;
+
 public class Jurnal03 {
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+        String a[] = new String[n];
+
+        for(int i=0;i<n;i++){
+            a[i] = s.next();
+        }
+
+        String key = s.next();
+
+        // sort dulu ASC (syarat binary search)
+        for(int i=1;i<n;i++){
+            String tmp = a[i];
+            int j=i-1;
+            while(j>=0 && a[j].compareTo(tmp) > 0){
+                a[j+1] = a[j];
+                j--;
+            }
+            a[j+1] = tmp;
+        }
+
+        // binary search
+        int l=0,r=n-1,mid,pos=-1;
+
+        while(l<=r){
+            mid = (l+r)/2;
+            if(a[mid].equals(key)){
+                pos = mid;
+                break;
+            } else if(a[mid].compareTo(key) < 0){
+                l = mid+1;
+            } else {
+                r = mid-1;
+            }
+        }
+
+        if(pos!=-1){
+            System.out.println("Ditemukan di indeks ke-" + pos);
+        } else {
+            System.out.println("Data tidak ditemukan");
+        }
     }
 }
